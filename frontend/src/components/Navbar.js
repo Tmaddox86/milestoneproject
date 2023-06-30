1c970fccac75943f7e219a4a7346f6a06c7125df
Tara Maddox <TMaddox85@gmail.com>
2023-06-29 18:54:39 -0700

    added login/signup pages/buttons/routes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,14 @@ const Navbar = () => {
                 <Link to="/">
                     <h1>Small Patient List</h1>
                 </Link>
+
+                <nav>
+                    <div>
+                        <Link to = "/login">Login</Link>
+                        <Link to = "/signup">Signup</Link>
+                    </div>
+                </nav>
+
             </div>
         </header>
     )
