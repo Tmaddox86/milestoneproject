cfad89f481f37f73c0144d5d3ec9df983a84232c
Tara Maddox <TMaddox85@gmail.com>
2023-06-29 17:57:02 -0700

    setup authContext to dispacth info

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { PatientsContextProvider } from './context/PatientsContext'
+import { AuthContextProvider } from './context/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <PatientsContextProvider>
     <App />
     </PatientsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
