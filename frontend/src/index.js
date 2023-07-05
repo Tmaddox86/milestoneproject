d09bfff77dde54866767b45ea741cd237188bb3c
Tara Maddox <TMaddox85@gmail.com>
2023-07-05 07:28:52 -0700

    changed indentation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-    <PatientsContextProvider>
-    <App />
-    </PatientsContextProvider>
+      <PatientsContextProvider>
+        <App />
+      </PatientsContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
